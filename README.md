1f3c857d07e754cec34797a535d773c530f44dd3
Hamza Louis <hamzalouis@Hamzas-MacBook-Air.local>
2026-02-09 21:34:16 +0300

    add variables and basic types

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https://frontendmasters.com/courses/complete-go/
 - The compiler needs to look into package with the name main and a main function
 - The compiler will not compile the code if you have unused imports in a Go file
 - The purpose of the package `main` in a Go application, is to create executable binaries
+- Unused variables aren't allowed in Go
+- Printf needs a new line specifier ("\n") where Println doesn't
--- main.go
+++ main.go
@@ -6,4 +6,30 @@ import (
 
 func main() {
 	fmt.Println("Hello Hamza")
+	// Unused variables aren't allowed in Go
+
+	var name string = "Hamza"
+	fmt.Printf("This is my name: %s\n", name)
+
+	age := 27
+	fmt.Printf("This is my age: %d\n", age)
+
+	var city string
+	city = "Amman"
+	fmt.Printf("This is my city: %s\n", city)
+
+	var country, continent string = "Jordan", "Asia"
+	fmt.Printf("%s is based in %s which is in %s\n", city, country, continent)
+
+	// To declare different variables with different types
+	var (
+		isEmployed bool = true
+		salary int = 2500
+		position string = "DevOps"
+	)
+	fmt.Printf("%s employed: %t, his salary equal %d and his position is %s\n", name, isEmployed, salary, position)
+
+	// Zero values
+	var defaultInt int
+	fmt.Println(defaultInt)
 }
